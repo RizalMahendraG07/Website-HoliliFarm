de16b96c2082f5e44732f62479e4c588cdfd2514
iqbalansh <alanshoriiqbal@gmail.com>
2025-05-26 23:57:09 +0700

    realtime api

--- app/Models/SensorReading.php
+++ app/Models/SensorReading.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SensorReading extends Model
+class SensorReading extends BaseModel
 {
     
     protected $table = 'sensor_readings';
